23c4ab4dcc504e5d71c25f109be200501d9555f4
Tushar Sethiya <tusharsethiya023@gmail.com>
2026-09-05 16:27:52 +0530

    fixed routes and redeployed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Har request ke saath automatically token bhej do (agar hai toh)
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),tailwindcss()],
+
   preview: {
+    host: '0.0.0.0',
     allowedHosts: ['arenax-4yzn.onrender.com'],
   },
 })
